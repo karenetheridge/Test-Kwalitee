c7049b9c62b1413359d4aed38f0f65626d8f9758
Karen Etheridge <ether@cpan.org>
2016-10-13 18:17:51 -0700

    increment $VERSION after 1.23 release

--- lib/Test/Kwalitee.pm
+++ lib/Test/Kwalitee.pm
@@ -5,7 +5,7 @@ package Test::Kwalitee;
 # ABSTRACT: Test the Kwalitee of a distribution before you release it
 # KEYWORDS: testing tests kwalitee CPANTS quality lint errors critic
 
-our $VERSION = '1.23';
+our $VERSION = '1.24';
 
 use Cwd ();
 use Test::Builder 0.88;
