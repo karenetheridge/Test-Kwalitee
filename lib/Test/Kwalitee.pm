7ffcea4750cd4861fe19d48f80215cadddf8c8c1
Karen Etheridge <ether@cpan.org>
2017-05-11 13:58:03 +0200

    improve the xt detection regexp, as per RT#120824

--- lib/Test/Kwalitee.pm
+++ lib/Test/Kwalitee.pm
@@ -45,8 +45,8 @@ sub kwalitee_ok
         # Please DO NOT enable this test to run for users, as it can fail
         # unexpectedly as parts of the toolchain changes!
         unless $ENV{_KWALITEE_NO_WARN} or $ENV{AUTHOR_TESTING} or $ENV{RELEASE_TESTING}
-            or (caller)[1] =~ m{^\./xt}
-            or ((caller)[0]->isa(__PACKAGE__) and (caller(1))[1] =~ m{^\./xt});
+            or (caller)[1] =~ m{^(?:\.[/\\])?xt\b}
+            or ((caller)[0]->isa(__PACKAGE__) and (caller(1))[1] =~ m{^(?:\.[/\\])?xt\b});
 
     my @run_tests = grep { /^[^-]/ } @tests;
     my @skip_tests = map { s/^-//; $_ } grep { /^-/ } @tests;
